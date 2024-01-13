46a20d911bfde0c6093610c7761342c311552a2a
DenisRaineri <denispraineri@gmail.com>
2024-01-13 15:12:19 -0300

    first commit

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,44 @@
+import { useContext } from 'react';
+import { Header } from '../../components/Header';
+import { Summary } from '../../components/Summary';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { SearchForm } from './components/SearchForm';
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles';
+import { priceFormatter } from '../../utils/formatter';
+
+export function Transactions() {
+  const { transactions } = useContext(TransactionsContext);
+
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
+}
